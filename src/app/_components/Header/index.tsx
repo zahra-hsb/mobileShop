508303f63afd74ba2406fe89e525f9cd3139b4bb
zahra-hsb <zahra16toa@gmail.com>
2024-10-19 02:31:57 -0700

    header created

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -34,7 +34,7 @@ const Header = () => {
                         <div className={`justify-between items-center w-full lg:flex lg:w-auto lg:order-1 ${isOpen ? 'flex' : 'hidden'}`} id="mobile-menu-2">  
                             <ul className="flex flex-col gap-5 mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">  
                                 <li>  
-                                    <a href="#" className="block py-2 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">صفحه اصلی</a>  
+                                    <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">صفحه اصلی</a>  
                                 </li>  
                                 <li>  
                                     <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">تماس با ما</a>  
