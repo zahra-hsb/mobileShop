46afafbfd85944ce2ad3365deb6b4c27cfe424a0
zahra-hsb <zahra16toa@gmail.com>
2024-10-19 02:28:21 -0700

    header created

---
+++ src/app/_components/MainPage/index.tsx
@@ -0,0 +1,9 @@
+const MainPage = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default MainPage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,6 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
       },
     },
   },
